10ef6abc70ad560faf1d90f10cdd618a713c6472
Wayne Sun <wtse.sun@gmail.com>
2019-05-15 15:55:37 -0700

    Setup structure of client and webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: __dirname + "/client/src/index.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist/'
+  }
+};
