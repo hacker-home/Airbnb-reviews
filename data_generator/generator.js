a95cec3883a007e883cdec702d851a6719d8616a
Wayne Sun <wtse.sun@gmail.com>
2019-05-15 19:11:05 -0700

    implement review generator

--- data_generator/generator.js
+++ data_generator/generator.js
@@ -1,12 +1,16 @@
-/* Airbnb fake reviews generator
-Library: casual (npm install casual)
-Input: Number of reviews
-Output: An array of random reviews
-*/
+/* 
+ * Airbnb fake reviews generator
+ * Library: casual (npm install casual)
+ * Input: Number of reviews
+ * Output: An array of random reviews
+ */
 
 var casual = require('casual');
 
 var generator = function (numberOfData) {
+
+  console.log('generating ' + numberOfData + ' of review(s)...')
+  
   res = [];
 
   for (let i = 0; i < numberOfData; i++) {
@@ -20,45 +24,49 @@ var generator = function (numberOfData) {
     curData['date'] = date;
 
     //sentence
-    var sentence = casual.sentence;
+    var sentence = casual.text;
     curData['sentence'] = sentence;
     
     //rating_accuracy
-    var accuracy = casual.double(from = 0, to = 5).toFixed(1);
-    curData['accuracy'] = Number(accuracy);
+    var accuracy__rating = casual.double(from = 0, to = 5).toFixed(1);
+    curData['accuracy__rating'] = Number(accuracy__rating);
     
     //rating_communication
-    var communication = casual.double(from = 0, to = 5).toFixed(1);
-    curData['communication'] = Number(communication);
+    var communication__rating = casual.double(from = 0, to = 5).toFixed(1);
+    curData['communication__rating'] = Number(communication__rating);
     
     //rating_cleanliness
-    var cleanliness = casual.double(from = 0, to = 5).toFixed(1);
-    curData['cleanliness'] = Number(cleanliness);
+    var cleanliness_rating = casual.double(from = 0, to = 5).toFixed(1);
+    curData['cleanliness_rating'] = Number(cleanliness_rating);
     
     //rating_location
-    var location = casual.double(from = 0, to = 5).toFixed(1);
-    curData['location'] = Number(location);
+    var location_rating = casual.double(from = 0, to = 5).toFixed(1);
+    curData['location_rating'] = Number(location_rating);
     
     //rating_check_in
-    var check_in = casual.double(from = 0, to = 5).toFixed(1);
-    curData['check_in'] = Number(check_in);
+    var check_in_rating = casual.double(from = 0, to = 5).toFixed(1);
+    curData['check_in_rating'] = Number(check_in_rating);
     
     //rating_value
-    var value = casual.double(from = 0, to = 5).toFixed(1);
-    curData['value'] = Number(value);
+    var value_rating = casual.double(from = 0, to = 5).toFixed(1);
+    curData['value_rating'] = Number(value_rating);
     
     //overall_rating = ratings above / 6
-    curData['overall_rating'] = Number(((curData.accuracy
-       + curData.communication 
-       + curData.cleanliness 
-       + curData.location 
-       + curData.check_in 
-       + curData.value) / 6)
+    curData['overall_rating'] = Number(((curData.accuracy__rating
+       + curData.communication__rating 
+       + curData.cleanliness_rating 
+       + curData.location_rating 
+       + curData.check_in_rating 
+       + curData.value_rating) / 6)
        .toFixed(1));
     //listing_id = 
     res.push(curData);
   }
+
+  console.log(numberOfData + ' reviews are ready.')
+
   return res;
 }
 
-console.log(generator(1));
+
+module.exports = { generator };
