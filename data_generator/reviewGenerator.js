f19cf3109b99cb0a62c450007b64227640040e47
Wayne Sun <wtse.sun@gmail.com>
2019-05-25 10:57:43 -0700

    Implement profile picture

--- data_generator/reviewGenerator.js
+++ data_generator/reviewGenerator.js
@@ -20,6 +20,18 @@ const reviewGenerator = function (numberOfData) {
     const name = casual.first_name;
     curData.name = name;
 
+    //gender 0:women 1:men
+    let from = 0;
+    let to = 1;
+    const gender = casual.integer(from, to);
+    curData.gender = gender;
+
+    //profile pic
+    from = 0;
+    to = 99;
+    const profilePicNum = casual.integer(from, to);
+    curData.profilePicNum = profilePicNum;
+
     // date
     let formate = 'MMMM YYYY';
     const date = casual.date(formate);
@@ -30,8 +42,8 @@ const reviewGenerator = function (numberOfData) {
     curData.sentence = sentence;
 
     // rating_accuracy
-    let from = 0;
-    let to = 5;
+    from = 0;
+    to = 5;
     const accuracy_rating = casual.double(from, to).toFixed(1);
     curData.accuracy_rating = Number(accuracy_rating);
 
