be5c0b168f8ea4de1de079d4e30b3f788ee61e15
Wayne Sun <wtse.sun@gmail.com>
2019-07-07 12:50:33 -0700

    adjusted test of date formate

--- test/test.js
+++ test/test.js
@@ -17,8 +17,8 @@ test('Should generate a random name', () => {
 
 test('Should generate a date matches formate YYYY-MMMM', () => {
   let date = reviewGenerator(1)[0].date;
-  let year = date.split('-')[0];
-  let month = date.split('-')[1];
+  let year = date.split(' ')[1];
+  let month = date.split(' ')[0];
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   expect(year.length).toBe(4);
   expect(months.indexOf(month)).toBeGreaterThanOrEqual(0);
